6ead77e9b0e3d219790e94dbbf4582a8270e4540
jaunecitron <jordanstreet9@hotmail.com>
2016-01-22 12:53:56 +0100

    pstl

---
+++ Documents/PSTL/src/point.ml
@@ -0,0 +1,8 @@
+open Printf
+
+type point = {x:float;y:float}
+
+let distance p1 p2 = sqrt((p2.y -. p1.y)*.(p2.y -. p1.y) +.
+		       (p2.x -. p1.x)*.(p2.x -. p1.x))
+
+let point_to_string p = sprintf "(%f;%f)" p.x p.y
