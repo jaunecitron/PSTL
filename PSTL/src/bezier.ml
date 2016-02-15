3c599836ec08301ec4436f64d209627cd9cba7fa
jaunecitron <jordanstreet9@hotmail.com>
2016-02-15 09:07:48 +0100

    bezier

--- PSTL/src/bezier.ml
+++ PSTL/src/bezier.ml
@@ -13,22 +13,29 @@ let coefficient_binomial k n =
 let polynome_bernstein i n t =
   (float_of_int (coefficient_binomial i n)) *. (t**(float_of_int i)) *. ((1.-.t)**(float_of_int (n-i)))
 
+									  
 let bezier t l =
   let n = (List.length l) - 1 in
-  let res_x = ref 0. in
-  let res_y = ref 0. in
-  let i = ref 0 in
+  let res = {x=0.;y=0.} in
+  let i = ref 0 in							
   let rec bez l =
     match l with
-       [] -> ()
-      |head::tail-> res_x := (polynome_bernstein !i n t) *. head.x +. !res_x;
-	      res_y := (polynome_bernstein !i n t) *. head.y +. !res_y;
-	      i := !i + 1;
-	      bez tail in
+       [] -> res
+      |head::tail-> res.x <- (polynome_bernstein !i n t) *. head.x +. res.x;
+		    res.y <- (polynome_bernstein !i n t) *. head.y +. res.y;
+		    i := !i + 1;
+		    bez tail in
   bez l
-									  
+				 
+    
 let _ =
   print_string (sprintf "C(3 8) %d\n" (coefficient_binomial 3 8));
   print_string (sprintf "3! %d\n" (factorielle 3));
   print_string (sprintf "%f\n" (3.**5.));
+  let p1 = {x=0.0;y=0.0} in
+  let p2 = {x=0.0;y=10.0} in
+  let p3 = {x=10.0;y=10.0} in
+  let p4 = {x=10.0;y=0.0} in
+  let polygone = p4::p3::p2::p1::[] in
+  print_string (point_to_string (bezier 1. polygone))
   
--- PSTL/src/test.ml
+++ PSTL/src/test.ml
@@ -58,27 +58,3 @@ let _ =
   prog := List.append (program_end cur_env) !prog;
   print_string (program_to_string !prog)
     
-    
-      
-  
-       
-       (*
-let _ =
-  let cur_env = env_init in
-  let prog = add_instruction (move_to cur_env 20.0 20.0) [] in
-  print_string (program_to_string prog);
-  let p1 = {x=0.0;y=0.0} in
-  let p2 = {x=0.0;y=10.0} in
-  let p3 = {x=10.0;y=10.0} in
-  let p4 = {x=10.0;y=0.0} in
-  let polygone = p4::p3::p2::p1::[] in
-  let prog_final = ref prog in
-  while cur_env.height < 10. do
-    prog_final := List.append (print_polygone cur_env polygone) !prog_final;
-    prog_final := add_instruction (lift cur_env) !prog_final;
-    (*print_string (env_to_string cur_env);*)
-  done;
-  print_string (program_to_string !prog_final)
-	*)
-
-
